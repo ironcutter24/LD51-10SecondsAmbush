09383daac2823df8b16934b8d83e75c3199f9a70
Damiano <taglia24@hotmail.it>
2022-10-01 22:38:58 +0200

    Bullet and spawner types

--- Assets/Scripts/BulletSpawner.cs
+++ Assets/Scripts/BulletSpawner.cs
@@ -1,14 +1,18 @@
+using DG.Tweening;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class BulletSpawner : MonoBehaviour
 {
-    [SerializeField] GameObject normalBulletPrefab;
+    [SerializeField] GameObject bulletPrefab;
+    [SerializeField] Material bulletMat;
+    [SerializeField] GameObject shootVFX;
 
     void Start()
     {
         StartCoroutine(_SpawnBullets());
+        shootVFX.GetComponentInChildren<SpriteRenderer>().material = bulletMat;
     }
 
     IEnumerator _SpawnBullets()
@@ -21,7 +25,13 @@ public class BulletSpawner : MonoBehaviour
             Vector2 dir = CharacterController.Instance.transform.position - transform.position;
             dir.Normalize();
 
-            var bullet = Instantiate(normalBulletPrefab, transform.position, Quaternion.identity).GetComponent<Bullet>();
+            const float flashDuration = .2f;
+            shootVFX.transform.localScale = Vector3.zero;
+            shootVFX.transform.DOScale(Vector3.one * 3f, flashDuration);
+            yield return new WaitForSeconds(flashDuration);
+            shootVFX.transform.DOScale(Vector3.zero, flashDuration);
+
+            var bullet = Instantiate(bulletPrefab, transform.position, Quaternion.identity).GetComponent<Bullet>();
             bullet.SetDirection(dir);
         }
     }
